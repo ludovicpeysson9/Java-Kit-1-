72deabd997693ec86b0c47a83591ca6f2536a770
ludovicpeysson9 <ludovic.peysson@le-campus-numerique.fr>
2022-12-12 13:20:58 +0100

    kit java 1 terminé

--- src/Exos.java
+++ src/Exos.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Exos {
     // Fonction qui print tous les entiers jusqu'à la limite avec une boucle for
     public void enumerate_for(int limit) { // Définition de fonction : public définit la visibilité de la fonction, void mentionne qu'elle ne renvoie rien
@@ -56,8 +58,17 @@ public class Exos {
         }
     }
 
-    // Fonction qui
+    // Fonction qui définit une suite de fibonacci à partir de 0 selon une limite donnée
     public void fibo(int limit){
+        if(limit == 0){
+            System.out.println("Mettre une limit supérieure à 0 svp");
+            return;
+        } else if (limit == 1) {
+            System.out.println(0);
+            System.out.println(1);
+            return;
+        }
+
         int[] fiboArray = new int[limit];
         fiboArray[0] = 0;
         fiboArray[1] = 1;
@@ -71,10 +82,103 @@ public class Exos {
             fiboArray[i] = futurNumber;
         }
         for(int affichage = 0; affichage < limit; affichage++){
-                System.out.println(fiboArray[affichage]);
+            System.out.println(fiboArray[affichage]);
         }
     }
 
+    // Fonction qui vérifie la marge d'erreur epsilon (division de la paire de la suite - phi doit être inférieur à
+    // epsilon en valeur absolue).  Si oui afficher la paire
 
+    public void approximation_fibo(float epsilon){
+        Integer a = 0;
+        int previousNumber = 1;
+        int actualNumber = 1;
+        double phi = 1.618033;
+        double verificationNumber = 1;
 
+        while(verificationNumber > epsilon){
+            actualNumber = actualNumber+previousNumber;
+            previousNumber = actualNumber-previousNumber;
+            verificationNumber = ( (double) actualNumber/ (double) previousNumber)-phi;
+            if(verificationNumber < 0 ){
+                verificationNumber = verificationNumber*-1;
+            }
+        }
+        System.out.println(actualNumber);
+        System.out.println(previousNumber);
+    }
+
+    // Fonction qui affiche chaque ligne d'une ArrayList de String grâce à la méthode toString()
+    public void describe(){
+
+        ArrayList<String> prenoms = new ArrayList<String>();
+
+        prenoms.add("Jean");
+        prenoms.add("Ahmed");
+        prenoms.add("Lea");
+        prenoms.add("Blanco");
+        prenoms.add("Philippe");
+
+        System.out.println(prenoms.toString());
+    }
+
+    // Fonction qui affiche chaque ligne d'une ArrayList de String grâce à une boucle foreach
+
+    public void describe_for_each(){
+
+        ArrayList<String> prenoms = new ArrayList<String>();
+
+        prenoms.add("Jean");
+        prenoms.add("Ahmed");
+        prenoms.add("Lea");
+        prenoms.add("Blanco");
+        prenoms.add("Philippe");
+
+        for(String prenom : prenoms){
+            System.out.println(prenom);
+        }
+    }
+
+    // Fonction qui affiche chaque ligne d'une ArrayList de String grâce à une boucle foreach si sa longueur est supérieure à 4
+
+    public void describe_for_each_sup4(){
+
+        ArrayList<String> prenoms = new ArrayList<String>();
+
+        prenoms.add("Jean");
+        prenoms.add("Ahmed");
+        prenoms.add("Lea");
+        prenoms.add("Blanco");
+        prenoms.add("Philippe");
+
+        for(String prenom : prenoms){
+            if (prenom.length() > 4 ) {
+                System.out.println(prenom);
+            }
+        }
+    }
+
+    // Fonction qui affiche chaque ligne d'une ArrayList de String en MAJUSCULES!
+
+    public void to_upper_case(){
+
+        ArrayList<String> prenoms = new ArrayList<String>();
+
+        prenoms.add("Jean");
+        prenoms.add("Ahmed");
+        prenoms.add("Lea");
+        prenoms.add("Blanco");
+        prenoms.add("Philippe");
+
+        for(String prenom : prenoms){
+            System.out.println(prenom.toUpperCase());
+        }
+    }
+
+    public void decrypt(){
+
+        String messageCrypte = "000T000000000000000000000000000000o0000u00000000000j0o0000000u0000000000r00s0000 c00o0000d00000e000000000000r000000000000000000 00000c0000000000o000m00000000000000m0e000 000000000s00i0000000000 0l000e00000000 g000000000000a00000000000000000r0s0000 000000000000000000000000000q00000u0000000000000000000000000i00000000 000f000i0ni00000r0000000a 000p00000a00000r0000000000000000000000000000000 m00000000000000a000in0t000000e00n000000000i00r0000000000000000 00000000000v00000000000o0000000t00r0000000e000000000 0c000o00d000000000e000000000 00e0000000000000000000000000000000000s0t0000000000000000000 0000000000000u0n00000 0000000000000000000000000000000ps00000000y000000000000000000c0h0000000op00000a0t0000h0e0 000000000000000v00000000000i00000000000000000000000000000000000000o0000l0000en00t 0000000q0u000000000i 00000000sa00000it00000 000où0000000000 000000v0000000o0000000000u000000000000000s000000000000000000 00000v00000000000000i00000000v0000000000000000000000000000e0000000000z0000000 00(000000c0i0000000000000000000ta0ti0o00000000n 000000d000000000000000000e00 00J000o00000000000000h0n00 0W00o000000000000o00000000000d0000000s0000000000)0";
+        messageCrypte = messageCrypte.replaceAll("0","");
+        System.out.println(messageCrypte);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,13 @@ public class Main {
         Scanner scanner = new Scanner(System.in); // Création de l'objet Scanner
 
 //        System.out.println("Ecrire un entier en limite"); // Instruction pour l'user
-        System.out.println("Ecrire un entier");
+//        System.out.println("Ecrire un entier");
+//        System.out.println("Ecrire un float entre 0 et 1 avec une VIRGULE");
 
 //        int entermaxlimit = scanner.nextInt(); // Déclaration et stockage dans une variable de l'entrée de l'utilisateur
 //        int n = scanner.nextInt();
-        int limit = scanner.nextInt();
+//        int limit = scanner.nextInt();
+//        float epsilon = scanner.nextFloat();
 
         Exos counter = new Exos(); // Création de l'objet counter à partir de la classe Exos
 //        counter.enumerate_for();
@@ -18,6 +20,12 @@ public class Main {
 //        counter.enumerate_for(entermaxlimit); // Appel de méthodes de counter définies dans la classe Exos
 //        counter.smallest_square_next_of_n(n);
 //        counter.greatest_square_previous_of_n(n);
-        counter.fibo(limit);
+//        counter.fibo(limit);
+//        counter.approximation_fibo(epsilon);
+//        counter.describe();
+//        counter.describe_for_each();
+//        counter.describe_for_each_sup4();
+//        counter.to_upper_case();
+        counter.decrypt();
     }
 }
